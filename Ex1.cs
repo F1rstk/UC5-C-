019dee56e8b5464087d9d7faa6b4c2b8bf06fe22
Filipe <filipe.sdiniz@senacsp.edu.br>
2024-06-05 17:20:08 -0300

    Inciando as aulas de C#

---
+++ Ex1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string nome;
+        int idade;
+        double n1;
+        double n2;
+
+
+
+        Console.WriteLine("Digite seu Nome:");
+        nome = Console.ReadLine();
+
+
+
+        Console.WriteLine("Olá, " + nome);
+        Console.WriteLine("Quantos anos você tem?");
+        idade = int.Parse(Console.ReadLine());
+
+
+
+        Console.WriteLine("Legal agora digite um número inteiro de 0 a 10");
+        n1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Agora digite mais um número inteiro de 0 a 10");
+        n2 = double.Parse(Console.ReadLine());
+
+
+
+        Console.WriteLine("Então, " + nome + " de " + idade + " anos, você digitou os números " + n1 + " e " + n2);
+        Console.WriteLine("A soma dos números é: " + (n1 + n2));
+        Console.WriteLine("A subtração dos números é: " + (n1 - n2));
+        Console.WriteLine("A Multiplicação dos números é: " + (n1 * n2));
+        Console.WriteLine("A Divisão dos números é: " + (n1 / n2));
+        Console.WriteLine(n1.ToString());
+    }
+}
